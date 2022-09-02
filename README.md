1febdebe740b3b7288f50be5dd5db650783206af
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-09-02 23:05:18 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+## Template for the new aitorlancharro.com
+
+<p>© 2022 Aitor Lancharro.  Code with ☕️</p>
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,33 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
+import Avatar from "../public/avatar.png";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <Image width="300px" height="200px" src={Avatar} />
+        <title>Aitor Lancharro - FullStack Developer | UX/UI Design</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Aitor Lancharro</h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          FullStack <code>Developer</code> | UX/UI Design
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <div className="card">
+            <h3>New Website coming soon!</h3>
+            <p> 🚧 🚧 🚧 🚧 </p>
+          </div>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+        <p>© 2022 Aitor Lancharro. Code with ☕️</p>
       </footer>
 
       <style jsx>{`
@@ -103,7 +74,7 @@ export default function Home() {
         }
 
         .title a {
-          color: #0070f3;
+          color: #ffd700;
           text-decoration: none;
         }
 
@@ -163,8 +134,8 @@ export default function Home() {
         .card:hover,
         .card:focus,
         .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+          color: #ffd700;
+          border-color: #ffd700;
         }
 
         .card h3 {
@@ -176,6 +147,7 @@ export default function Home() {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
+          text-align: center;
         }
 
         .logo {
@@ -205,5 +177,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
