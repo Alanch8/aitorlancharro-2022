66dbf934ffec8e2c9eb770f386b81905344d6c97
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-12 16:35:09 +0100

    Tailwind CSS added

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
       <main>
         <div>
           <img src="/avatar.svg" alt="avatar" className="avatar"></img>
