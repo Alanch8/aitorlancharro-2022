374d8f8202997810ed66a3a7e84ab30abe7d32a6
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-01-14 17:01:52 +0100

    folder updated to 23 to solve problems

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "/Users/alanch8/Dev/aitorlancharro-2022/styles/globals.css";
+import "/Users/alanch8/Dev/aitorlancharro-2023/styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
