8a27b8786e3553420db75f5d5e99d51b94aacd05
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-01-14 17:33:38 +0100

    problems with tailwind solved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import "/Users/alanch8/Dev/aitorlancharro-2023/styles/globals.css";
+// import "/Users/alanch8/Dev/aitorlancharro-2023/styles/style.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
