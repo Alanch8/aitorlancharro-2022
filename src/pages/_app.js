c5e3976d28d41532e6f76c0f209884a3b26d9a91
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-03-02 11:31:38 +0100

    css rute mod

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "/Users/alanch8/Dev/aitorlancharro-2023/styles/globals.css";
+import "../../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
